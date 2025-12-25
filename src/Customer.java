3063f40055accbe5a00e1e74a2581c4ff61c6989
sacenovaldiyar <aldiyarsacenov3@gmail.com>
2025-12-25 13:13:14 +0500

    first commit

--- src/Customer.java
+++ src/Customer.java
@@ -3,10 +3,29 @@ public class Customer {
     private double balance;
     public Customer(String name, double balance) {
         this.name = name;
-        this.balance = balance;}
+        this.balance = balance;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
     public boolean canBuy(double price) {
-        return balance >= price;}
+        return balance >= price;
+    }
+
     public void pay(double price) {
-        balance -= price;}
+        balance -= price;
+    }
+
     public void showInfo() {
-        System.out.println(name + "    Balance: " + balance);}}
+        System.out.println(name + "    Balance: " + balance);
+    }
+}
--- src/product.java
+++ src/product.java
@@ -6,6 +6,18 @@ public class product {
         this.name = name;
         this.price = price;
         this.quantity = quantity;}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public double getPrice() {
         return price;}
     public int getQuantity() {
