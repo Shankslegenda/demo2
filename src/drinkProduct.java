459b21c4b7b4be77c81a2e8551891f9a1a5b4065
sacenovaldiyar <aldiyarsacenov3@gmail.com>
2026-01-08 12:48:45 +0500

    first commit

--- src/drinkProduct.java
+++ src/drinkProduct.java
@@ -1,24 +1,19 @@
 public class drinkProduct extends Product {
-
     private boolean cold;
     private double volume;
-
     public drinkProduct(int id, String name, double price, int stock, boolean cold, double volume) {
         super(id, name, price, stock);
         this.cold = cold;
         this.volume = volume;
     }
-
     @Override
     public void work() {
         System.out.println(name + " is a drink product. Cold: " + cold + ", Volume: " + volume + "L");
     }
-
     @Override
     public String getCategory() {
         return "Drink";
     }
-
     public boolean isCold() { return cold; }
     public double getVolume() { return volume; }
 }
